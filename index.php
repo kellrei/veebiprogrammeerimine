20a500af34dee3108d229d69dd06a4fe5ef34b15
Kelly Reinmaa <kellrei@users.noreply.github.com>
2018-09-11 14:22:22 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Kelly Reinmaa, õppetöö</title>
+</head>
+
+<body>
+<h1>Kelly Reinmaa</h1>
+<p>See leht on valminud <a href="https://www.tlu.ee/">TLÜ</a> õppetöö raames ja ei oma mõtestatud või muul moel väärtuslikku sisu. 
+<br>1234</br></p>
+<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone">
+<p>Minu sõber teeb ka <a href="../../~karemin/" target="_blank">veebi</a></p>
+</body>
+
+</html> 
